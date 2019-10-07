3977f61cd1cb0a2280de45d36d32c7f3ef3e3dbf
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-07 13:40:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(start_num)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(start_num)
+  n = start_num
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
